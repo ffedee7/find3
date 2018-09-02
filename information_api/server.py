ee34918a527b2fe67e2eef01f0a4f4e5d5ec052f
Federico Grunwald <federico@tryolabs.com>
2018-09-02 19:55:53 -0300

    added information API to expose to the museum client

---
+++ information_api/server.py
@@ -0,0 +1,65 @@
+import logging
+from flask import Flask, request, jsonify
+from sql_layer import add_piece, get_all_pieces, get_piece
+
+
+# create logger with 'spam_application'
+logger = logging.getLogger('information')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('information_api.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - [%(name)s/%(funcName)s] - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+app = Flask(__name__)
+
+
+
+@app.route('/pieces', methods=['GET'])
+def get_all():
+    pieces = get_all_pieces()
+    return jsonify({
+        'success': True,
+        'pieces': pieces
+    })
+
+@app.route('/pieces/<piece_id>', methods=['GET'])
+def get(piece_id):
+    return jsonify({
+        'success': True,
+        'pieces': get_piece(piece_id)
+    })
+
+# location_name, description, audio_url, image_url
+@app.route('/pieces', methods=['POST'])
+def add():
+    piece_json = request.json()
+    required_fields = ["location_name", "description", "audio_url", "image_url"]
+    for field in required_fields:
+        if field not in piece_json:
+            return jsonify({
+                'success': False,
+                'message': f"Field {field} is missing!"
+            })
+    try:
+        add_piece(piece_json)
+        return jsonify({
+            'success': True,
+            'message': "Piece created succesfully!"
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'message': str(e)
+        })
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ information_api/sql_layer.py
@@ -0,0 +1,61 @@
+import psycopg2
+
+db_connection = psycopg2.connect("postgres://posifi:posifiposifi@posifi-db.c5jlfkn2l4jz.sa-east-1.rds.amazonaws.com/posifi")
+
+def create_tables():
+    cursor = db_connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE piece(
+            piece_id serial PRIMARY KEY,
+            location_name TEXT,
+            description TEXT,
+            audio_url TEXT,
+            image_url TEXT
+        );
+        """
+    )
+    db_connection.commit()
+    cursor.close()
+
+def add_piece(piece_dict):
+    cursor = db_connection.cursor()
+    cursor.execute(
+        """
+        INSERT INTO piece (location_name, description, audio_url, image_url) VALUES(%s, %s, %s, %s);
+        """,
+        (
+            piece_dict['location_name'],
+            piece_dict['description'],
+            piece_dict['audio_url'],
+            piece_dict['image_url']
+        )
+    )
+    db_connection.commit()
+    cursor.close()
+
+def get_all_pieces():
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM piece;")
+    all_pieces = cursor.fetchall()
+    pieces = []
+    for raw_piece in all_pieces:
+        pieces.append(serialize_piece(raw_piece))
+
+    return pieces
+
+def get_piece(piece_id):
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM piece WHERE piece_id = %s;", (piece_id,))
+    raw_piece = cursor.fetchone()
+
+    return serialize_piece(raw_piece)
+
+def serialize_piece(raw_piece):
+    return {
+        "piece_id": raw_piece[0],
+        "location_name": raw_piece[1],
+        "description": raw_piece[2],
+        "audio_url": raw_piece[3],
+        "image_url": raw_piece[4]
+    }
